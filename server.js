1da0bdb721b96556f0d35ceac4f8a84afed03c0a
DominikKoniarz <47055121+DominikKoniarz@users.noreply.github.com>
2023-06-11 20:53:34 +0200

    Modified DB connection
    Using knex to connect to database

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const app = express();
 const { nanoid } = require("nanoid");
-const mysql = require("mysql");
+const knex = require("knex")(require("./config/knexConfig"));
 
-const dbCredentials = require("./config/dbCredentials");
 const serviceConfig = require("./config/serviceConfig");
 const urlPattern = serviceConfig.urlPattern;
 const PORT = serviceConfig.PORT;
 const BASE_URL = serviceConfig.BASE_URL;
-
-let conn = mysql.createConnection(dbCredentials);
-conn.connect();
 
 app.use(express.json());
 
@@ -24,35 +20,34 @@ app.use((req, res, next) => {
 app.use("/", express.static("./public"));
 
 // Acces site by minidfied url
-app.get("/:url", (req, res) => {
+app.get("/:url", async (req, res) => {
 	if (!req?.params?.url) return res.sendStatus(404);
 
 	const requestedURL = BASE_URL + req.params.url;
 
 	try {
-		const queryString = `SELECT original_url, visit_counter FROM sites WHERE short_url=?`;
+		const result = await knex("sites")
+			.select("original_url", "visit_counter")
+			.where("short_url", requestedURL);
 
-		conn.query(queryString, [requestedURL], (err, results, fields) => {
-			if (err)
-				return res.status(500).json({ message: "Internal Server Error!" });
+		const originalURL = result[0]?.original_url;
+		if (!originalURL) return res.sendStatus(404);
 
-			const originalURL = results[0]?.original_url;
-			if (!originalURL) return res.sendStatus(404);
-			const visit_counter = results[0]?.visit_counter;
+		const visit_counter = result[0]?.visit_counter + 1;
 
-			const incCounterQuery =
-				"UPDATE sites set visit_counter=? WHERE short_url=?";
+		// const updateResult =
+		await knex("sites")
+			.update({
+				visit_counter: visit_counter,
+			})
+			.where({
+				short_url: requestedURL,
+			});
 
-			conn.query(
-				incCounterQuery,
-				[visit_counter + 1, requestedURL],
-				(err, results, fields) => {
-					if (err) return res.sendStatus(500);
+		// if (!updateResult)
+		// 	return res.status(500).json({ message: "Internal Server Error!" });
 
-					res.status(301).redirect(originalURL);
-				}
-			);
-		});
+		res.status(301).redirect(originalURL);
 	} catch (e) {
 		console.log(e.stack);
 		return res.status(500).json({ message: "Internal Server Error!" });
@@ -60,7 +55,7 @@ app.get("/:url", (req, res) => {
 });
 
 // Handle minifing URL path
-app.post("/shorten", (req, res) => {
+app.post("/shorten", async (req, res) => {
 	if (!req?.body?.orgURL)
 		return res.status(400).json({ message: "URL is required!" });
 	if (!urlPattern.test(req.body.orgURL)) {
@@ -68,20 +63,19 @@ app.post("/shorten", (req, res) => {
 	}
 
 	let originalURL = req.body.orgURL;
-	let shortURL = BASE_URL + nanoid();
+	const shortURL = BASE_URL + nanoid();
 
 	if (!originalURL.includes("http://") && !originalURL.includes("https://"))
 		originalURL = "https://" + originalURL;
 
 	try {
-		const queryString = `INSERT INTO sites VALUES(NULL, ?, ?, 0)`;
-
-		conn.query(queryString, [originalURL, shortURL], (err, results, fields) => {
-			if (err)
-				return res.status(500).json({ message: "Internal Server Error!" });
-
-			res.status(201).json({ originalURL: originalURL, shortURL: shortURL });
+		await knex("sites").insert({
+			original_url: originalURL,
+			short_url: shortURL,
+			visit_counter: 0,
 		});
+
+		res.status(201).json({ originalURL: originalURL, shortURL: shortURL });
 	} catch (e) {
 		console.log(e.stack);
 		return res.status(500).json({ message: "Internal Server Error!" });
@@ -101,10 +95,4 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}!`);
-});
-
-conn.on("error", (err) => {
-	console.log(err);
-
-	conn = mysql.createConnection(dbCredentials);
 });
